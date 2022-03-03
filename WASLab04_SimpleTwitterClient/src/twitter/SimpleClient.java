82418256883851ffcb85b00e9be40c05aec2ef0d
PeiLi <PeiLi@DESKTOP-VJOQUAJ.mshome.net>
2022-03-03 11:53:16 +0100

    tasca #5 finalitzada

--- WASLab04_SimpleTwitterClient/src/twitter/SimpleClient.java
+++ WASLab04_SimpleTwitterClient/src/twitter/SimpleClient.java
@@ -2,6 +2,8 @@ package twitter;
 
 import java.util.Date;
 
+import twitter4j.Paging;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
@@ -13,8 +15,12 @@ public class SimpleClient {
 		final Twitter twitter = new TwitterFactory().getInstance();
 		
 		Date now = new Date();
-		String latestStatus = "Hey @fib_was, we've just completed task #4 [timestamp: "+now+"]";
-		Status status = twitter.updateStatus(latestStatus);
-		System.out.println("Successfully updated the status to: " + status.getText());       
+		//String latestStatus = "Hey @fib_was, we've just completed task #4 [timestamp: "+now+"]";
+		//Status status = twitter.updateStatus(latestStatus);
+		ResponseList<Status> idLatTweet = twitter.getUserTimeline("@fib_was");
+		Status t = idLatTweet.get(0);
+		Long id = t.getId();
+		twitter.retweetStatus(id);
+		System.out.println(t.getText());       
 	}
 }
